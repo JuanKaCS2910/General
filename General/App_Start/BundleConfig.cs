6a04fa34f4622b2ba5eead15387750907e9ebae6
Juan Carlos Castro Socla <juan.castro.socla@gmail.com>
2019-08-20 22:29:46 -0500

    Subir los proyectos.

--- General/App_Start/BundleConfig.cs
+++ General/App_Start/BundleConfig.cs
@@ -8,24 +8,64 @@ namespace General
         // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            
+            // CSS style (bootstrap/inspinia)
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.min.css", "~/Content/animate.css",
+                      "~/Content/style.css", "~/Content/override.css"));
+
+            // Font Awesome icons
+            bundles.Add(new StyleBundle("~/font-awesome/css").Include(
+                      "~/fonts/font-awesome/css/font-awesome.min.css", new CssRewriteUrlTransform()));
+
+            //jQuery
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            // jQueryUI CSS
+            bundles.Add(new ScriptBundle("~/Scripts/plugins/jquery-ui/jqueryuiStyles").Include(
+                        "~/Scripts/plugins/jquery-ui/jquery-ui.min.css"));
 
-            // Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
-            // para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            // jQueryUI 
+            bundles.Add(new StyleBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/plugins/jquery-ui/jquery-ui.min.js"));
 
+            // Bootstrap
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/popper.min.js", "~/Scripts/bootstrap.min.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            // Inspinia script
+            bundles.Add(new ScriptBundle("~/bundles/inspinia").Include(
+                      "~/Scripts/plugins/metisMenu/jquery.metisMenu.js",
+                      "~/Scripts/plugins/pace/pace.min.js", "~/Scripts/app/inspinia.js"));
+
+            // SlimScroll
+            bundles.Add(new ScriptBundle("~/plugins/slimScroll").Include(
+                      "~/Scripts/plugins/slimscroll/jquery.slimscroll.min.js"));
+
+            // validate 
+            bundles.Add(new ScriptBundle("~/plugins/validate").Include(
+                      "~/Scripts/plugins/validate/jquery.validate.min.js"));
+
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //         "~/Content/animate.css",
+            //         "~/Content/style.css",
+            //         "~/Content/bootstrap.min.css",
+            //         "~/Content/override.css"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            //            "~/Scripts/jquery.validate*"));
+
+            //// Utilice la versión de desarrollo de Modernizr para desarrollar y obtener información. De este modo, estará
+            //// para la producción, use la herramienta de compilación disponible en https://modernizr.com para seleccionar solo las pruebas que necesite.
+            //bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            //            "~/Scripts/modernizr-*"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js"));
+
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css",
+            //          "~/Content/site.css"));
         }
     }
 }
---
+++ Utility/Utilities.cs
@@ -0,0 +1,37 @@
+﻿namespace Utility
+{
+    using Domain.Entities.General;
+    using System.Net;
+    using System.Net.Mail;
+    using System.Threading.Tasks;
+    using System.Web.Configuration;
+
+    public static class Utilities
+    {   
+        public static async Task EnviarCorreo(SendEmail email)
+        {
+            var message = new MailMessage();
+            message.To.Add(new MailAddress(email.To));
+            message.From = new MailAddress(WebConfigurationManager.AppSettings["AdminUser"]);
+            message.Subject = email.Subject;
+            message.Body = email.Body;
+            message.IsBodyHtml = true;
+
+            using (var smtp = new SmtpClient())
+            {
+                var credentials = new NetworkCredential
+                {
+                    UserName = WebConfigurationManager.AppSettings["AdminUser"],
+                    Password = WebConfigurationManager.AppSettings["AdminPassWord"]
+                };
+                smtp.Credentials = credentials;
+                smtp.Host = WebConfigurationManager.AppSettings["SMTPName"];
+                smtp.Port = int.Parse(WebConfigurationManager.AppSettings["SMTPPort"]);
+                smtp.EnableSsl = true;
+                await smtp.SendMailAsync(message);
+
+            }
+        }
+
+    }
+}
