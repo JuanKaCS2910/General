8c26b4971950ab8eb061222f119a9b6e684bb19c
Juan Carlos Castro Socla <juan.castro.socla@gmail.com>
2019-08-21 21:24:09 -0500

    Navbar, UnitOfWork, Repository.

---
+++ Repository/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data;
+
+namespace Repository.UnitOfWork
+{
+    public partial class UnitOfWork //: IDisposable
+    {
+        //private BDFisioterapiaEntities context = new BDFisioterapiaEntities("BDFisioterapiaEntities");
+
+        //public void Save()
+        //{
+        //    context.SaveChanges();
+        //}
+
+        //public void Save(Action action)
+        //{
+        //    using (var transaction = context.Database.BeginTransaction(IsolationLevel.ReadCommitted))
+        //    {
+        //        try
+        //        {
+        //            action();
+        //            transaction.Commit();
+        //        }
+        //        catch (Exception e)
+        //        {
+        //            transaction.Rollback();
+        //            throw e;
+        //        }
+        //    }
+        //}
+
+        //private bool disposed = false;
+
+        //protected virtual void Dispose(bool disposing)
+        //{
+        //    if (!this.disposed)
+        //    {
+        //        if (disposing)
+        //        {
+        //            context.Dispose();
+        //        }
+        //    }
+        //    this.disposed = true;
+        //}
+
+        //public void Dispose()
+        //{
+        //    Dispose(true);
+        //    GC.SuppressFinalize(this);
+        //}
+    }
+}
---
+++ Repository/UnitOfWork/UnitOfWorkReference.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.UnitOfWork
+{
+    public partial class UnitOfWorkReference //: IDisposable
+    {
+
+        //#region Private
+        //private GenericRepository<Aplicaciones> aplicacionesRepository;
+        //private GenericRepository<Persona> personaRepository;
+        //#endregion
+
+        //#region Public
+        //public GenericRepository<Aplicaciones> AplicacionesRepository
+        //{
+        //    get
+        //    {
+
+        //        if (this.aplicacionesRepository == null)
+        //        {
+        //            this.aplicacionesRepository = new GenericRepository<Aplicaciones>(context);
+        //        }
+        //        return aplicacionesRepository;
+        //    }
+        //}
+
+        //public GenericRepository<Persona> PersonaRepository
+        //{
+        //    get
+        //    {
+
+        //        if (this.personaRepository == null)
+        //        {
+        //            this.personaRepository = new GenericRepository<Persona>(context);
+        //        }
+        //        return personaRepository;
+        //    }
+        //}
+        //#endregion
+
+    }
+}
